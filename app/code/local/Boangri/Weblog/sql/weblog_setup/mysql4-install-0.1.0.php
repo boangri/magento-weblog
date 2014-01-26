e437d01f28fe224280e8cd63196c67ea98a36722
Boris Gribovskiy <xinu@yandex.ru>
2014-01-26 17:45:11 +0400

    version 0.1.0, updated install script

--- app/code/local/Boangri/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
+++ app/code/local/Boangri/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
@@ -16,12 +16,13 @@ $installer->run("
       `blogpost_id` int(11) NOT NULL auto_increment,
       `title` text,
       `post` text,
+      `status` tinyint default 1,
       `date` datetime default NULL,
       `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
       PRIMARY KEY  (`blogpost_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-    INSERT INTO `{$installer->getTable('weblog/blogpost')}` VALUES (1,'My New Title','This is a blog post','2009-07-01 00:00:00','2009-07-02 23:12:30');
+    INSERT INTO `{$installer->getTable('weblog/blogpost')}` VALUES (1,'My New Title','This is a blog post',1,'2009-07-01 00:00:00','2009-07-02 23:12:30');
 ");
 $installer->endSetup();
 
