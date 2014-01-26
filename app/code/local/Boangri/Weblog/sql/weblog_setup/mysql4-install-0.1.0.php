6cb7cd2b71343091be867c7e43ab82f482dd2699
Boris Gribovskiy <xinu@yandex.ru>
2014-01-26 18:21:08 +0400

    default value for status changed (1->2)

--- app/code/local/Boangri/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
+++ app/code/local/Boangri/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
@@ -22,7 +22,7 @@ $installer->run("
       PRIMARY KEY  (`blogpost_id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-    INSERT INTO `{$installer->getTable('weblog/blogpost')}` VALUES (1,'My New Title','This is a blog post',1,'2009-07-01 00:00:00','2009-07-02 23:12:30');
+    INSERT INTO `{$installer->getTable('weblog/blogpost')}` VALUES (1,'My New Title','This is a blog post',2,'2009-07-01 00:00:00','2009-07-02 23:12:30');
 ");
 $installer->endSetup();
 
